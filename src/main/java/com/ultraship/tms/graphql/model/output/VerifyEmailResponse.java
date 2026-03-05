417bfbf71ac31c04583856d09404fc15b2b403ec
smondalkreeti <subhodip.mondal@kreeti.com>
2026-03-05 23:48:02 +0530

    Getter added for graphql mapping

--- src/main/java/com/ultraship/tms/graphql/model/output/VerifyEmailResponse.java
+++ src/main/java/com/ultraship/tms/graphql/model/output/VerifyEmailResponse.java
@@ -1,8 +1,10 @@
 package com.ultraship.tms.graphql.model.output;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class VerifyEmailResponse {
     private String verifiedEmail;
     private boolean success;
